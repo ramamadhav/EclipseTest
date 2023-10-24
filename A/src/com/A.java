ae255bc919181aadbc8e44a32d71486958cf62c8
ramamadhav <nitinhasule1@gmail.com>
2023-10-24 20:43:24 +0530

    A class 8 line changes

--- A/src/com/A.java
+++ A/src/com/A.java
@@ -5,7 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("welcome");
-		System.out.println("hello ji");
+		System.out.println("hello ji") ;
 	}
 
 }
